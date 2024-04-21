fce89ac03e9f7cb258e79ac6a30745360c293ef2
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-04-21 21:58:06 +0200

    parsing templates

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ import {createElement} from 'wju-dzej-es/instance/VNodeGenerator.js'
 console.log('Hello, world!');
 
 const rootComponentConfig = {
-    render() {
-        /*
-        <div>
-        <h1>{{message}}</h1>
-        <p v-on-click="sayHello">{{reversedMessage}}</p>
-        </div>
-         */
-        return createElement('div', {},
-            [
-                createElement('h1', {}, [this.message]),
-                createElement('p', {on: {click: this.sayHello}, 'id':'abcd'}, [
-                    this.reversedMessage
-                ]
-                ),
-                createElement('button', {on: {click: this.incrementCounter}}, [this.counter])
-            ]
-        );
-    },
+    template:`<div>
+        <h1>{{this.message}}</h1>
+        <p v-on-click="this.sayHello" id="abcd">{{this.reversedMessage}}</p>
+        <button v-on-click="this.incrementCounter">{{this.counter}}</button>
+        <p>{{this.counter2}}</p>
+        </div>`,
+    // render() {
+    //     /*
+    //     <div>
+    //     <h1>{{message}}</h1>
+    //     <p v-on-click="sayHello">{{reversedMessage}}</p>
+    //     </div>
+    //      */
+    //     return createElement('div', {},
+    //         [
+    //             createElement('h1', {}, [this.message]),
+    //             createElement('p', {on: {click: this.sayHello}, 'id':'abcd'}, [
+    //                 this.reversedMessage
+    //             ]
+    //             ),
+    //             createElement('button', {on: {click: this.incrementCounter}}, [this.counter]),
+    //             createElement('p', {}, [this.counter2])
+    //         ]
+    //     );
+    // },
     data() {
         return {
             message: 'Hello, world!',
@@ -40,9 +47,18 @@ const rootComponentConfig = {
     computed: {
         reversedMessage() {
             return this.message.split('').reverse().join('');
+        },
+        counter2(){
+            return this.counter.toFixed(2);
+        }
+    },
+    watch: {
+        message() {
+            console.log('message się zmienił')
         }
     }
 }
 
 const rootComponent=createWjuInstance(rootComponentConfig);
 rootComponent.$mount(document.body)
+window.dbg=rootComponent;
